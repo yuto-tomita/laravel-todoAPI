3b8ef514b7f0e9a928f6fbe73e8ec3988f5624d1
yuto-tomita <qualidea01@gmail.com>
2021-02-06 18:08:02 +0900

    素のSQLでユーザーを作成できるように修正

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\{Validator, Hash, DB};
-
+use Illuminate\Support\Facades\{Validator, Hash, DB, Mail};
+use App\Mail\OrderShipped;
+use App\Models\Order;
+use Illuminate\Auth\Events\Registered;
+use Carbon\Carbon;
 class UserController extends Controller
 {
     public function signup(Request $request) {
@@ -14,10 +17,26 @@ class UserController extends Controller
         $this->validator($request->all())->validate();
  
         $user = new User();
-        // DBに保存
-        $user->fill($request->all())->save();
+        clock($user);
 
-        return response()->json(['success' => '成功！'], 201);
+        DB::insert(
+            'insert into users (name, email, password, created_at, updated_at) values (?, ?, ?, ?, ?)',
+            [$request->name, $request->email, $request->password, Carbon::now(), Carbon::now()]
+        );
+
+        $token = $user->createToken('token-name');
+        $token->planeTextToken;
+
+        // $user->fill($request->all())->save();
+        // メールを送信できるようにするもの。メールから別途email_verified_atを更新するメソッドを書く必要あり。
+        // event(new Registered($user));
+        // メール送れるようにするもの
+        // Mail::to($request->email)->send(new OrderShipped());
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user
+        ], 201);
     }
 
     public function signin(Request $request) {
--- routes/api.php
+++ routes/api.php
@@ -6,25 +6,21 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Auth\Middleware\EnsureEmailIsVerified;
 
-// Route::group(['middleware' => ['api']], function () {
-// });
+Route::group(['middleware' => 'api'], function () {
+	Route::post('signup', 'App\Http\Controllers\Api\UserController@signup');
 
-// Route::group(['middleware' => 'api'], function () {
-// });
-Route::post('signup', 'App\Http\Controllers\Api\UserController@signup');
+	Route::get('/email/verify', function () {
+		return view('auth.verify-email');
+	})->middleware(['auth'])->name('verification.notice');
+	
+	Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+		$request->fulfill();
+	})->middleware(['auth', 'signed'])->name('verification.verify');
+});
 Route::post('signin', 'App\Http\Controllers\Api\UserController@signin');
 
 // Route::get('profile', function () {
 // 	// 確認済みユーザーのときだけ実行されるコード…
 // })->middleware('verified');
 
-Route::get('/email/verify', function () {
-	return view('auth.verify-email');
-})->middleware(['auth'])->name('verification.notice');
-
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-	$request->fulfill();
-
-	return redirect('/home');
-})->middleware(['auth', 'signed'])->name('verification.verify');
 
